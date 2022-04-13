2dd750d12cbd96ae49f786b8c29ea42ba39039fc
Bilal Aslam <bilalaslam0355@gmail.com>
2022-04-13 16:36:34 +0500

    Chamges Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = React.useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 13335000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <h1 class="main-heading">Testimonials Slider</h1>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, company, quote } = person;
+
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
+          return (
+            <article className={position} key={id}>
+              <div className="container silder-items d-flex justify-content-center">
+                <div>
+                  <img src={image} alt={name} className="person-img" />
+                </div>
+                <div className="text-left ml-4">
+                  <h4 className="fw-bold mb-2">{name}</h4>
+                  <h4 className="mb-2">{title}</h4>
+                  <h4 className="mb-3">{company}</h4>
+
+                  <p className="text">{quote}</p>
+                  <FaQuoteRight className="icon" />
+                </div>
+              </div>
+            </article>
+          );
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,48 @@
+const people = [
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+    name: "maria ferguson",
+    title: "office manager",
+    company: "Cash Converters Henderson",
+    quote:
+      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.Product design is the process designers use to blend user needs with business goals to help brands make consistently successful products.",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "john doe",
+    title: "Store Mangager",
+    company: "Cash Converters Henderson",
+    quote: `I feel lucky to be a part of a business who understands and takes care of its people
+      I have not only grown professionally, but also developed personally. I have overcome 
+      hurdles to stand where I am today. It’s simple; if you work hard, you can achieve your 
+      goals. There is nothing you can’t achieve if you set your mind to it!”
+      `,
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
+    name: "peter smith",
+    title: "product designer",
+    company: "Cash Converters Henderson",
+    quote:
+      "Product design is the process designers use to blend user needs with business goals to help brands make consistently successful products. Product designers work to optimize the user experience in the solutions they make for their users—and help their brands by making products sustainable for longer-term business needs.",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "susan andersen",
+    title: "the boss",
+    company: "Cash Converters Henderson",
+
+    quote:
+      "Product design is the process designers use to blend user needs with business goals to help brands make consistently successful products. Product designers work to optimize the user experience in the solutions they make for their users—and help their brands by making products sustainable for longer-term business needs. ",
+  },
+];
+
+export default people;
